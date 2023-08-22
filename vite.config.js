5d5f891ae9a45ed5e08ed8998ffa55aa514a269a
mitesh-7span <mitesh@7span.com>
2023-08-22 13:59:54 +0530

    pages renamed

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,13 @@ import { defineConfig } from "vite";
 export default defineConfig({
   build: {
     minify: false,
+    polyfill: false,
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-        nested: resolve(__dirname, "1.html"),
-        nested2: resolve(__dirname, "2.html"),
-        nested3: resolve(__dirname, "3.html"),
+        nested: resolve(__dirname, "list-of-pages.html"),
+        nested2: resolve(__dirname, "join-mentorship.html"),
+        nested3: resolve(__dirname, "JRclick2success.html"),
       },
     },
   },
